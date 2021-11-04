1573febd0fce83c9989293b87292e6a09fc353a1
masahiro96848 <feetwcpic9436@gmail.com>
2021-11-04 23:51:23 +0900

    vuexのローカルストレージ保存を導入途中まで

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,29 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-	state: {
+const savedPosts = localStorage.getItem('board-posts')
 
+const store =  new Vuex.Store({
+	state: {
+		posts: savedPosts ? JSON.parse(savedPosts):[
+
+		]
 	},
 	mutations: {
+		addList(state, payload) {
+			state.posts.push({
+				name: payload.name,
+				post: payload.post
+			})
+		}
 	},
 	actions: {
+		addList(context, payload) {
+			context.commit('addList', payload)
+		}
 	},
-	modules: {
+	getters: {
 	}
 })
+
+export default store
